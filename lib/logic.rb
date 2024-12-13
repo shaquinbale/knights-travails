77961b0a8038f2c746ad5c6c5ae7a8261a38d7f6
shaquinbale <scopelenskingdra@gmail.com>
2024-12-13 11:28:42 -0500

    Fix all bugs, finish project
    Many misc. issues. The main one being that
    moves_to_parent was adding to the visited array,
    not the history array

--- lib/logic.rb
+++ lib/logic.rb
@@ -3,7 +3,11 @@ require_relative 'knight'
 class Logic
   def knight_moves(current_position, destination)
     destination_node = move_search(current_position, destination)
-    moves_to_parent(destination_node, current_position)
+    moves = moves_to_parent(destination_node, current_position)
+
+    puts "You made it in #{moves.length} moves! Here's your path:"
+    p current_position
+    moves.each { |move| p move}
   end
 
   # Returns a node at the position of the destination with corresponding history
@@ -11,56 +15,23 @@ class Logic
     queue = [Knight.new(position)]
     visited = []
 
-    until queue.empty?
-     current_node = queue.shift
-     next if visited.include? current_node.position
-     return current_node if current_node.position == destination
-     
-     current_node.generate_children
-     current_node.children.each { |child| visited << child }
+    until queue.empty?   
+      current_node = queue.shift
+      next if visited.include? current_node.position
+      return current_node if current_node.position == destination
+      
+      current_node.generate_children
+      current_node.children.each { |child| queue << child }
     end
   end
 
   def moves_to_parent(child_node, parent_pos)
-     history = []
-     current_node = child_node
-     until current_node.position == parent_pos
-       history << current_node.position
-       current_node = current_node.parent
-     end
-     history
+      history = []
+      current_node = child_node
+      until current_node.position == parent_pos
+        history.unshift current_node.position
+        current_node = current_node.parent
+      end
+      history
   end
 end
-
-game = Logic.new
-
-game.knight_moves([], [])
-
-=begin 
-
-def knight_moves(current_position, destination)
-  destination_node = move_search(current_position, destination)
-  moves_to_parent(destination_node, current_position)
-end
-
-
-def move_search(position, destination)
-  initialize and queue a knight node at position
-  queue = []
-  visited = []
-
-  until queue.empty?
-    dequeue a node and check it. return node if #position == destination
-    queue its #generate_children
-  end
-
-  raise error: move not found
-end
-
-def moves_to_parent(child_node, parent_pos)
-  history = []
-  current_node = child_node
-  until child_node.position == parent_pos
-    history << current_node.position
-    current_node = current_node.parent
-=end
