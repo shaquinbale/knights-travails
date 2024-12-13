ad755af7d6cc0650c65d5aacce7b4354559bbe5c
shaquinbale <scopelenskingdra@gmail.com>
2024-12-13 11:10:30 -0500

    Fix bugs in the Knight class
    Parents weren't being generated correctly and
    i decided to change KNIGHT_MOVES from a class
    variable to a constant
    
    KNIGHT_MOVES was also being modified accidentally

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,30 +1,27 @@
-require_relative 'node'
-
 class Knight
   attr_reader :position
-  attr_accessor :parents, :children
+  attr_accessor :parent, :children
 
-  @@KNIGHT_MOVES = [ # An array of possible [row, column] transformations for potential moves
+  KNIGHT_MOVES = [ # An array of possible [row, column] transformations for potential moves
     [2, 1], [2, -1], # Up 2, left/right 1
     [-2, 1], [-2, -1], # Down 2, left/right 1
     [1, 2], [-1, 2], # Right 2, up/down 1
     [1, -2], [-1, -2]  # Left 2, up/down 1
-  ]
+].freeze
 
-  def initialize(position)
-    @position = position # A array of two integers (i.e. [2, 4])
-    @parents = []
+  def initialize(position, parent=nil) 
+    @position = position # All positions in this project will be an array of two ints between 0 and 7 (i.e. [2, 4])
+    @parent = parent
     @children = []
   end
 
   def valid_moves(position=@position)
     x, y = position
-    moves = @@KNIGHT_MOVES.map { |dx, dy| [dx += x, dy += y] }
+    moves = KNIGHT_MOVES.map { |dx, dy| [x + dx, y + dy] }
     moves.select { |nx, ny| nx.between?(0, 7) && ny.between?(0, 7) }
   end
 
   def generate_children
-    valid_moves.each { |move| @children << Knight.new(move) }
+    valid_moves.each { |move| @children << Knight.new(move, self) }
   end
 end
-
